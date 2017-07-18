86464b2f749f720f9769eee97b54bc73a08ed45e
Francisco Fernandes <francisco.fernandes.j@gmail.com>
2017-07-17 23:19:13 -0300

    Added definition of test cases for project functions

---
+++ tests.py
@@ -0,0 +1,39 @@
+import unittest as ut
+
+class TestUserCommands(ut.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_initialize_matrix(self):
+        pass
+
+    def test_clear_matrix(self):
+        pass
+
+    def test_lay_value_at(self):
+        pass
+
+    def test_vertical_values(self):
+        pass
+
+    def test_horizontal_values(self):
+        pass
+
+    def test_key_in_rect(self):
+        pass
+
+    def test_fill_region(self):
+        pass
+
+    def test_save_matrix(self):
+        pass
+
+    def test_matrix_2_str(self):
+        pass
+
+
+if __name__ == '__main__':
+    ut.main()
