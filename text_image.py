83e7cba0293ac6130041bc80590a94d38aeb9fc1
Francisco Fernandes <francisco.fernandes.j@gmail.com>
2017-07-18 00:39:59 -0300

    Added empty event loop function

--- text_image.py
+++ text_image.py
@@ -136,6 +136,43 @@ def matrix_2_str(matrix):
     return ''.join(output)
 
 
+def event_loop():
+    """ Function that handles the user input
+
+    This project will receive a series of user inputs regarding the creation,
+    filling and manipulation of simple bidimentional matrices. This function
+    takes care of checking the validity of user input, provide feeback for
+    invalid values and calling the due functions for each command.
+
+    Commands:
+        I M N:      Initialize an empty (zero-valued) MxN ( colsXrows ) matrix.
+                    Here, the value zero is defined by the letter O not the
+                    number 0. Also, be aware that this command does not follow
+                    the common matrices dimension convention, which is rowsXcols
+
+        C:          Sets the current matrix values to zero
+
+        L X Y C:    Set matrix position (X,Y) with value C
+
+        V X Y1 Y2 C:Sets matrix positions (X, Y1-Y2) with value C
+
+        H X X1 Y2 C:Sets matrix positions (X1-X2, Y) with value C
+
+        K X1 Y1 X2 Y2 C : Sets a rectangular region, with bounds (X1,Y1) for
+                          top-left corner and (X2,Y2) to bottom-right corner
+
+        F X Y C : Fills a region with value C. A region is defined by the X,Y
+                  position with value C and neighbor zero valued (O) positions,
+                  both horizontaly and verticaly.
+
+        S filename: Saves the matrix to a file with name "filename"
+
+        X : Exits the program
+
+    """
+    pass
+
+
 def main():
     pass
 
