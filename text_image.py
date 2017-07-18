e1d092d1c5e0aabd078520fa2dd4b0c4d94d11b7
Francisco Fernandes <francisco.fernandes.j@gmail.com>
2017-07-17 23:22:46 -0300

    Added definitions for project functions

---
+++ text_image.py
@@ -0,0 +1,48 @@
+# !/usr/bin/python
+# -*- coding: utf8 -*-
+""" MODULE LEVEL DOCSTRING: WRITE SOMETHING HERE LATER
+"""
+
+
+def initialize_matrix(cols, rows):
+    pass
+
+
+def clear_matrix(matrix):
+    pass
+
+
+def lay_value_at(matrix, col, row, value):
+    pass
+
+
+def vertical_values(matrix, col, row_up, row_down, value):
+    pass
+
+
+def horizontal_values(matrix, col_left, col_right, row, value):
+    pass
+
+
+def key_in_rect(matrix, col_top, row_top, col_bottom, row_bottom, value):
+    pass
+
+
+def fill_region(matrix, col, row, value):
+    pass
+
+
+def save_matrix(matrix, filename):
+    pass
+
+
+def matrix_2_str(matrix):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
