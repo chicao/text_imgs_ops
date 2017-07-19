64f00e1aa1a7ace702e592bdf0c996a0689ba641
Francisco Fernandes <francisco.fernandes.j@gmail.com>
2017-07-19 08:46:50 -0300

    Rewrote test case to run tests on the TextImage object

--- tests.py
+++ tests.py
@@ -1,8 +1,30 @@
+""" Tests for image matrix operations
+
+The text image processing simulation requires processing of multidimensional
+lists. These operations are very sensitive to user input and proper
+function testing will result in better reliability of the image matrices
+manipulations.
+"""
+
 import unittest as ut
+from text_image import TextImage
 
-from text_processing import *
+class TestTextImgManipulations(ut.TestCase):
+    """ Test case for text image manipulations
 
-class TestUserCommands(ut.TestCase):
+    The setUp method is to ensure that each test will have a proper data
+    to use as test manipulation and matrix criation checking
+
+    Attributes:
+        empty_img (list(list)): An empty 2d list for population and matrix
+                                initialization checking
+        filled_img (list(list)): A filled 2d list for functions manipulations
+        region_img (list(list)): A partially filled 2d list for testing filling
+                                 regions
+        cols (int): column quantity
+        rows (int): rows quantity
+    """
+
     def setUp(self):
 
         self.empty_img = [['O','O','O','O'],
@@ -26,17 +48,26 @@ class TestUserCommands(ut.TestCase):
         self.cols = 5
         self.rows = 4
 
+        tmp_image = TextImage()
+        tmp_image.image = self.filled_img
+        tmp_image.cols = self.cols
+        tmp_image.rows = self.rows
+        self.image = tmp_image
+
     def tearDown(self):
         pass
 
     def test_initialize_matrix(self):
-        init = initialize_matrix(self.cols, self.rows)
-        self.assertTrue(init == self.empty_img)
+        self.image.initialize_matrix(self.cols, self.rows)
+        self.assertTrue(self.image.image == self.empty_img)
 
     def test_clear_matrix(self):
-        self.assertTrue(self.empty_img != self.filled_img)
-        clear_matrix(self.filled_img)
-        self.assertTrue(self.empty_img == self.filled_img)
+        self.image.image = self.filled_img
+        self.image.cols = self.cols
+        self.image.rows = self.rows
+
+        self.image.clear_matrix()
+        self.assertTrue(self.image.image == self.empty_img)
 
     def test_lay_value_at(self):
         pos = 3,2
@@ -46,8 +77,9 @@ class TestUserCommands(ut.TestCase):
                      ['Q','Q','Z','E'],
                      ['Q','T','P','R']]
 
-        lay_value_at(self.filled_img, pos[0]-1, pos[1]-1, 'C')
+        self.image.lay_value_at(pos[0]-1, pos[1]-1, 'C')
         self.assertTrue(self.filled_img == check_img)
+
 
     def test_vertical_values(self):
         col = 4
@@ -58,12 +90,11 @@ class TestUserCommands(ut.TestCase):
                      ['C','C','C','E'],
                      ['Q','T','P','R']]
 
-        vertical_values(self.filled_img,
-                        col-1,
-                        v_pos[0]-1,
-                        v_pos[1]-1,
-                        'C')
-        self.assertTrue(self.filled_img == check_img)
+        self.image.vertical_values(col-1,
+                                   v_pos[0]-1,
+                                   v_pos[1]-1,
+                                   'C')
+        self.assertTrue(self.image.image == check_img)
 
     def test_horizontal_values(self):
         row = 4
@@ -75,13 +106,12 @@ class TestUserCommands(ut.TestCase):
                      ['Q','Q','Z','C'],
                      ['Q','T','P','C']]
 
-        horizontal_values(self.filled_img,
-                        h_pos[0]-1,
-                        h_pos[1]-1,
-                        row-1,
-                        'C')
+        self.image.horizontal_values(h_pos[0]-1,
+                                     h_pos[1]-1,
+                                     row-1,
+                                     'C')
 
-        self.assertTrue(self.filled_img == check_img)
+        self.assertTrue(self.image.image == check_img)
 
     def test_key_in_rect(self):
         top = 2,3
@@ -93,14 +123,13 @@ class TestUserCommands(ut.TestCase):
                      ['Q','Q','C','C'],
                      ['Q','T','C','C']]
 
-        key_in_rect(self.filled_img,
-                    top[0]-1,
-                    top[1]-1,
-                    bottom[0]-1,
-                    bottom[1]-1,
-                    'C')
+        self.image.key_in_rect(top[0]-1,
+                                top[1]-1,
+                                bottom[0]-1,
+                                bottom[1]-1,
+                                'C')
 
-        self.assertTrue(self.filled_img == check_img)
+        self.assertTrue(self.image.image == check_img)
 
     def test_fill_region(self):
         pos = 4,3
@@ -117,17 +146,27 @@ class TestUserCommands(ut.TestCase):
                          ['I','C','C','C'],
                          ['I','C','C','C']]
 
-        fill_region(self.empty_img, pos[0], pos[1], 'C')
-        self.assertTrue(self.empty_img == all_filled)
+        self.image.image = self.empty_img
 
-        fill_region(self.region_img, pos[0], pos[1], 'C')
-        self.assertTrue(self.region_img == region_filled)
+        self.image.fill_region(pos[0],
+                               pos[1],
+                               'C')
+        self.assertTrue(self.image.image == all_filled)
 
-    def test_save_matrix(self):
-        pass
+
+        self.image.image = self.region_img
+        self.image.fill_region(pos[0],
+                               pos[1],
+                               'C')
+        self.assertTrue(self.image.image == region_filled)
 
     def test_matrix_2_str(self):
-        pass
+        converted = ('AZOQQ\n'
+                     'QABQT\n'
+                     'EGGZP\n'
+                     'OIEER\n')
+        output = self.image.image_2_str()
+        self.assertTrue(converted == output)
 
 
 if __name__ == '__main__':
